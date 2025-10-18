de0e69e35a0f6f9cbb3188d059f4accae1522936
roufunr <rouf@nevada.unr.edu>
2025-10-18 12:55:11 -0400

    refactored add two number using linkedlist

--- src/leetcode/meta/add_two_numbers_2/Solution.java
+++ src/leetcode/meta/add_two_numbers_2/Solution.java
@@ -13,46 +13,45 @@ class ListNode {
 
 
 public class Solution {
-	private ListNode reverseList(ListNode head) {
-		ListNode newHead = null;
-		ListNode itrNode = head;
-		while(itrNode != null) {
-			ListNode newNode = new ListNode(itrNode.val, null);
-			newNode.next = newHead != null ? newHead : null;
-			newHead = newNode;
-			itrNode = itrNode.next;
-		}
-		return newHead;
-	} 
-	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		ListNode head = null;
-		ListNode itrNode1 = l1;
-		ListNode itrNode2 = l2;
-		int carryIn = 0;
-		while(itrNode1 != null | itrNode2 != null) {
-			int sum = 0;
-			if(itrNode1 == null) {
-				sum += itrNode2.val + carryIn;
-				itrNode2 = itrNode2.next;
-			} else if(itrNode2 == null) {
-				sum += itrNode1.val + carryIn;
-				itrNode1 = itrNode1.next;
-			} else {
-				sum += itrNode1.val + itrNode2.val + carryIn;
-				itrNode1 = itrNode1.next;
-				itrNode2 = itrNode2.next;
-			}
-			ListNode newNode = new ListNode(sum % 10; null);
-			newNode.next = head != null ? head : null;
-			head = newNode;
-			carryIn = sum / 10;
-		}
-		if(carryIn > 0) {
-			ListNode newNode = new ListNode(carryIn; null);
-			newNode.next = head != null ? head : null;
-			head = newNode;
-		}	
-		return reverseList(head);
-	}
+    private ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        ListNode itrNode = head;
+        while(itrNode != null) {
+            ListNode newNode = new ListNode(itrNode.val, null);
+            newNode.next = newHead;
+            newHead = newNode;
+            itrNode = itrNode.next;
+        }
+        return newHead;
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null;
+        ListNode itrNode1 = l1;
+        ListNode itrNode2 = l2;
+        int carryIn = 0;
+        while(itrNode1 != null | itrNode2 != null) {
+            int sum = 0;
+            if(itrNode1 == null) {
+                sum += itrNode2.val + carryIn;
+                itrNode2 = itrNode2.next;
+            } else if(itrNode2 == null) {
+                sum += itrNode1.val + carryIn;
+                itrNode1 = itrNode1.next;
+            } else {
+                sum += itrNode1.val + itrNode2.val + carryIn;
+                itrNode1 = itrNode1.next;
+                itrNode2 = itrNode2.next;
+            }
+            ListNode newNode = new ListNode(sum % 10, null);
+            newNode.next = head;
+            head = newNode;
+            carryIn = sum / 10;
+        }
+        if(carryIn > 0) {
+            ListNode newNode = new ListNode(carryIn, null);
+            newNode.next = head;
+            head = newNode;
+        }
+        return reverseList(head);
+    }
 }
-
