41fb6684da80d3d854d00bb4c576d6b9a6030435
roufunr <rouf@nevada.unr.edu>
2025-10-25 11:25:58 -0400

    added consistent and precise recursive solution

---
+++ src/leetcode/meta/lowest_common_ancestor_of_binary_search_tree_235/SolutionV1.java
@@ -0,0 +1,20 @@
+package leetcode.meta.lowest_common_ancestor_of_binary_search_tree_235;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+ 
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else if(root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            return root;
+        }
+    }
+}
