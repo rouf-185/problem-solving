a7f18acb9da38a4f37e149e5f6ab7c0f66c03b4f
roufunr <rouf@nevada.unr.edu>
2025-10-25 06:21:31 -0400

    added solution

---
+++ src/leetcode/meta/contains_duplicate_II_219/Solution.java
@@ -0,0 +1,17 @@
+package leetcode.meta.contains_duplicate_II_219;
+
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(set.contains(nums[i])) {
+                return true;
+            } 
+            set.add(nums[i]);
+            if(set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
